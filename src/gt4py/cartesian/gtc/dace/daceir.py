97a66200e9dd47e3c3c4f94527ff4746d0e80dbd
Roman Cattaneo <>
2024-10-18 14:28:19 +0200

    Fix typing issue raised by gt4py tests

--- src/gt4py/cartesian/gtc/dace/daceir.py
+++ src/gt4py/cartesian/gtc/dace/daceir.py
@@ -851,11 +851,11 @@ class Tasklet(ComputationNode, IterationNode, eve.SymbolTableTrait):
 class DomainMap(ComputationNode, IterationNode):
     index_ranges: List[Range]
     schedule: MapSchedule
-    computations: List[Union[Tasklet, DomainMap, NestedSDFG]]
+    computations: List[Union[DomainMap, NestedSDFG, Tasklet]]
 
 
 class ComputationState(IterationNode):
-    computations: List[Union[Tasklet, DomainMap]]
+    computations: List[Union[DomainMap, NestedSDFG, Tasklet]]
 
 
 class DomainLoop(IterationNode, ComputationNode):
